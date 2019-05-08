9165e326ce76371fe3d91c5dc325077a3093b1f3
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-05-07 19:19:39 -0500

    login code complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,42 @@ class UsersController < ApplicationController
     erb :"users/login"
   end
 
+#purpose of this route is to receive the login form,
+#find the user and log the user in( create a session)
+  post '/login' do
+    #find the user
+    @user = User.find_by(:email => params[:email])
+    #authenticate the user - verify the user is who they say they are
+    #they have credientials - email/password
+    if @user.authenticate(params[:password])
+      #log in the user(create their session)
+      session[:user_id] = @user[:id]
+
+      #redirect the user to landing page
+      redirect "/users/#{@user[:name]}"
+
+    else
+      #tell the user they enter invalid credentials
+      #redirect them to login page
+      redirect '/login'
+
+    end
+
+  end
+
+
 #render over a signup page
   get '/signup' do
     erb :"users/signup"
   end
+
+  get '/users/:name' do
+    
+    erb :"users/show"
+
+  end
+
+
 
 
 end
