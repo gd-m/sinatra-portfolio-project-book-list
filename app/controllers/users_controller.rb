a21c120032c5e8a70736c8051c58b804500f9ee8
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-05-10 15:58:05 -0500

    added a route for user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,6 +58,7 @@ class UsersController < ApplicationController
 
   get '/users/:id/home' do
     @user = User.find_by(:id => session[:user_id])
+    @book_entries = BookEntry.all
     erb :"users/home"
 
   end
@@ -67,6 +68,11 @@ class UsersController < ApplicationController
     session.clear
     #redirect to welcome page
     redirect '/'
+  end
+
+  get '/users/:id/profile' do
+    @user = User.find_by(:id => params[:id])
+    erb :"users/profile"
   end
 
 
